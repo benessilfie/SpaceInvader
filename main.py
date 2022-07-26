84820771412a99388a070a27e6688cec10307660
essilfiequansah <bquansah007@gmail.com>
2022-07-26 17:33:11 +0000

    add new player

--- main.py
+++ main.py
@@ -12,48 +12,49 @@ icon = pygame.image.load('images/spaceship.png')
 pygame.display.set_icon(icon)
 
 # Add Player
-playerImg = pygame.image.load('images/player.png')
+playerImg = pygame.image.load('images/player2.png')
+playerImg = pygame.transform.scale(playerImg, (58, 58))
 playerX = 370
 playerY = 480
 playerX_change = 0
 # playerY_change = 0
 
+""" Function to draw the player on the screen """
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
-# Keep game in an infinite loop
+# Keep game in an infinite loop until closed
 running = True
 while running:
-    # Fill the screen with a color
+    """ Fill the screen with a background color """
     screen.fill((0, 0, 0))
 
+    """" add event to close game when user clicks the close button """
     for event in pygame.event.get():
-        """" Close game event for when 
-        the user clicks the close button """
         if event.type == pygame.QUIT:
             running = False
 
     """ add keystroke event for when the left or right keys are pressed """
     if event.type == pygame.KEYDOWN:
-        """ When the left arrow is pressed """
+        """ Player should move left when the left arrow is pressed """
         if event.key == pygame.K_LEFT:
             playerX_change = -0.3
 
-        """ When the right arrow is pressed """
+        """ Player should move right when the right arrow is pressed """
         if event.key == pygame.K_RIGHT:
             playerX_change = 0.3
 
     """ add keystroke event for when the left or right keys are released """
     if event.type == pygame.KEYUP:
-        """ Player shouldnt move on the x axis when 
-        left or right arrow is released """
+        """ Player should stop moving when left or right arrow is released """
         if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
             playerX_change = 0
 
+    """ Move the player on the screen """
     playerX += playerX_change
-    # playerY += playerY_change
 
-    # Draw the player on the screen
+    """ Draw the player on the screen """
     player(playerX, playerY)
 
+    """ Update the screen """
     pygame.display.update()
