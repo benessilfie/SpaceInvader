2e03d1ef03b48dedb73edfbc9c125ac9e28784ae
essilfiequansah <bquansah007@gmail.com>
2022-07-26 17:01:15 +0000

    add keystroke event for when the left or right keys are pressed

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('images/player.png')
 playerX = 370
 playerY = 480
-# playerX_change = 0
+playerX_change = 0
 # playerY_change = 0
 
 def player(x, y):
@@ -28,8 +28,30 @@ while running:
     screen.fill((0, 0, 0))
 
     for event in pygame.event.get():
+        """" Close game event for when 
+        the user clicks the close button """
         if event.type == pygame.QUIT:
             running = False
+
+    """ add keystroke event for when the left or right keys are pressed """
+    if event.type == pygame.KEYDOWN:
+        """ When the left arrow is pressed """
+        if event.key == pygame.K_LEFT:
+            playerX_change = -0.3
+
+        """ When the right arrow is pressed """
+        if event.key == pygame.K_RIGHT:
+            playerX_change = 0.3
+
+    """ add keystroke event for when the left or right keys are released """
+    if event.type == pygame.KEYUP:
+        """ Player shouldnt move on the x axis when 
+        left or right arrow is released """
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            playerX_change = 0
+
+    playerX += playerX_change
+    # playerY += playerY_change
 
     # Draw the player on the screen
     player(playerX, playerY)
